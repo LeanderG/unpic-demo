609d2dcabe749120b9a44a205d3331ad5f63c467
Leander Gilles <Leander@neugelb.com>
2023-03-08 19:22:19 +0100

    inital commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import netlify from "@astrojs/netlify/edge-functions";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  output: "server",
+  adapter: netlify(),
+});
